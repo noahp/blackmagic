08ea49eed790f7fae8d8e0b8f691b195b9e039a8
Noah Pendleton <noahp@noreply.github.com>
2020-12-11 14:07:22 -0500

    skip lpc43xx probe RIP

--- src/target/cortexm.c
+++ src/target/cortexm.c
@@ -445,7 +445,7 @@ bool cortexm_probe(ADIv5_AP_t *ap)
 			PROBE(lpc11xx_probe); /* LPC24C11 */
 			PROBE(lpc43xx_probe);
 		} else if (ap->ap_partno == 0x4c4) { /* Cortex-M4 ROM */
-			PROBE(lpc43xx_probe);
+			// PROBE(lpc43xx_probe);
 			PROBE(lpc546xx_probe);
 			PROBE(kinetis_probe); /* Older K-series */
 		}
